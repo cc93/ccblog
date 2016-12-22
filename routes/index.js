633d8bee37e6782f4372338e44b6b90cbc64a654
congcong <543281892@qq.com>
2016-12-22 09:59:02 +0800

    add dupont, newborn

--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,11 @@ module.exports = function (app) {
     app.use('/signout', require('./signout'));
     app.use('/posts', require('./posts'));
     app.use('/users', require('./users'));
+    app.get('/dupont', function (req, res) {
+        //sendFile(必须为绝对路径或者指定根目录), __dirname返回当前执行脚本所在目录的绝对路径
+        res.sendFile('./public/dupont/index.html', {root: __dirname + '/../'});
+    });
+    app.get('/newborn', function (req, res) {
+        res.sendFile('./public/newborn/index.html', {root: __dirname + '/../'});
+    });
 };
---
+++ test.js
@@ -0,0 +1,4 @@
+/**
+ * Created by congcong on 2016/12/22.
+ */
+console.log('__dirname = '+ __dirname);
